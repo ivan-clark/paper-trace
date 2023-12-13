e278517eb99a6b9a6ff74e9c75770be45a83d818
PLASMA\clark <gomzclark@gmail.com>
2023-12-14 03:47:44 +0800

    added new tables and entities

---
+++ backend/DataAccess/Entities/Timeline.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataAccess.Entities;
+
+public partial class Timeline
+{
+    public int Id { get; set; }
+
+    public int? TransactionId { get; set; }
+
+    public int? StatusId { get; set; }
+
+    public DateTime? CreatedDate { get; set; }
+}
---
+++ backend/DataAccess/Entities/Transaction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataAccess.Entities;
+
+public partial class Transaction
+{
+    public int Id { get; set; }
+
+    public int? SenderId { get; set; }
+
+    public int? RecepientId { get; set; }
+
+    public string? Subject { get; set; }
+
+    public string? Message { get; set; }
+}
