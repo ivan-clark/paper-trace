a762a7de936861cffbbf1649c460952ba2d76eee
PLASMA\clark <gomzclark@gmail.com>
2023-12-15 00:01:21 +0800

    get smtp key from env

--- backend/API/Program.cs
+++ backend/API/Program.cs
@@ -11,7 +11,7 @@ using sib_api_v3_sdk.Client;
 
 var builder = WebApplication.CreateBuilder(args);
 
-Configuration.Default.ApiKey.Add("api-key", builder.Configuration["SMTP:ApiKey"]);
+Configuration.Default.ApiKey.Add("api-key", Environment.GetEnvironmentVariable("SMTP_KEY"));
 
 // Auth
 builder.Services.AddAuthentication(auth => {
