ac092b0db7a1bc96398ff9a8931f46966eb7cc93
Rivera <billy100007@gmail.com>
2024-03-09 01:33:59 +0800

    Some minor Changes in Making the UserTransmitalModel as Nullable

--- backend/API/Models/UserTransmitalModel.cs
+++ backend/API/Models/UserTransmitalModel.cs
@@ -4,8 +4,8 @@ namespace API.Models
 {
     public class UserTransmitalModel
     {
-        public DocumentModel DocumentModel { get; set; }
-        public TransactionModel TransactionModel { get; set; }
-        public RouteModel RouteModel { get; set; }
+        public DocumentModel? DocumentModel { get; set; }
+        public TransactionModel? TransactionModel { get; set; }
+        public RouteModel? RouteModel { get; set; }
     }
 }
